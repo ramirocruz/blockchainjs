ea32bf8e89c1099d00e4ea04ef215606a70beb19
Ramirocruz <ramirocruz08@gmail.com>
2018-10-08 11:39:49 +0530

    added proof of work and transactions

--- bc.js
+++ bc.js
@@ -1,47 +1,106 @@
 const SHA256 = require('crypto-js').SHA256;
 
-class Block{
-    constructor(data,index=0,priviousHash='',timestamp='')
+class Transaction{
+    constructor(from,to,amount)
     {
-        this.index=index;
+        this.from=from;
+        this.to=to;
+        this.amount=amount;
+    }
+}
+class Block{
+    constructor(timestamp,transactions,previousHash)
+    {
         this.timestamp=timestamp;
-        this.data=data;
-        this.priviousHash=priviousHash;
-        this.currentHash=this.calculateHash();
+        this.transactions=transactions;
+        this.previousHash=previousHash;
+        this.nonce=0;
+        this.currentHash=this.calculateHash();        
     }
     calculateHash(){
 
-        return SHA256(this.index+this.timestamp+JSON.stringify(this.data)+this.priviousHash).toString();
+        return SHA256(this.index+this.timestamp+JSON.stringify(this.data)+this.previousHash + this.nonce ).toString();
     }
-   
+   mineNewBlock(difficulty)
+   {
+       while(this.currentHash.substr(0,difficulty) !== "0".repeat(difficulty))
+       {
+           this.nonce++;
+           this.currentHash=this.calculateHash();
+       }
+       console.log("A new block has been mined....");
+   }
 }
 // Date().toISOString().replace(/[.:-]/g,'')
 class Blockchain{
     constructor()
-    {
-        this.chain= [Block];
-        this.chain.pop();
-        this.getgenesisblock();
+    {   
+        this.systemaddress = SHA256("systemaddress").toString();
+        this.miningreward = 8;
+        this.difficulty = 1; 
+        this.chain= [this.getgenesisblock()];
+        this.pendingTransactions=[];
     }
     getgenesisblock()
     {    
-         this.chain.push(new Block("This is a genesis block",this.chain.length,SHA256("0").toString(),Date.now().toString()));
+         return new Block(Date.now(),"This is genesis block",SHA256("0").toString());
          
     }
-    addBlock(newBlock)
+    getlatestBlock()
     {
-        newBlock.index=this.chain.length;             
-        newBlock.priviousHash=this.chain[this.chain.length - 1].currentHash;
-        newBlock.timestamp=Date.now().toString();        
-        newBlock.currentHash=newBlock.calculateHash();
-        this.chain.push(newBlock);
+        return this.chain[this.chain.length - 1];
+    }
+  minePendingTransactions(miningrewardAdress)
+  {
+        let block = new Block(Date.now(),this.pendingTransactions,this.getlatestBlock().currentHash);
+        block.mineNewBlock(this.difficulty);
+        this.chain.push(block);
+        this.pendingTransactions=[new Transaction(this.systemaddress,miningrewardAdress,this.miningreward)];
+  }
+  queueTransactions(transaction){
+      this.pendingTransactions.push(transaction);
+  }
+  getBalanceOfAddress(address)
+  {
+      let balance = 0;
+      for(const block of this.chain)
+      {
+          for(const trans of block.transactions)
+          {
+              if(trans.from === address)
+              balance -= trans.amount;
+              else if(trans.to === address)
+              balance += trans.amount;
+
+          }
+      } 
+      return balance;
+  }
+    checkchainvalidation(){
+        for(let i=1;i<this.chain.length;i++)
+        {
+           
+            if(this.chain[i].previousHash !== this.chain[i-1].calculateHash())
+            return false;
+            
+            
+        }
+        return true;
     }
 }
+zcoin = new Blockchain();
+console.log(JSON.stringify(zcoin,null,4));
+let joiningbonus = new Transaction(zcoin.systemaddress,SHA256("raman").toString(),100);
+let trans1 = new Transaction(SHA256("raman").toString(),SHA256("cruz").toString(),50);
+let trans2 = new Transaction(SHA256("cruz").toString(),SHA256("raman").toString(),60);
+zcoin.queueTransactions(joiningbonus);
+zcoin.queueTransactions(trans1);
+zcoin.queueTransactions(trans2);
+zcoin.minePendingTransactions(SHA256("ramiro").toString());
+console.log("The balance of raman is:"+zcoin.getBalanceOfAddress(SHA256("raman").toString()));
+console.log("The balance of cruz is:"+zcoin.getBalanceOfAddress(SHA256("cruz").toString()));
+console.log("The balance of ramiro is:"+zcoin.getBalanceOfAddress(SHA256("ramiro").toString()));
+zcoin.minePendingTransactions(SHA256("ramiro").toString());
+console.log("The balance of ramiro again is:"+zcoin.getBalanceOfAddress(SHA256("ramiro").toString()));
 
-block1 = new Block({balance : 500});
-block2 = new Block({balance : 200});
-blockchain = new Blockchain();
-blockchain.addBlock(block1);
-blockchain.addBlock(block2);
-
-console.log(JSON.stringify(blockchain,null,4));
+console.log(JSON.stringify(zcoin,null,4));
